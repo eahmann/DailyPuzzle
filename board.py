14caba6397cf682c9566ad4f7cfdc5b4d7d474f8
Eric Ahmann <eric@ahmann.me>
2022-03-14 14:17:42 -0500

    some progress

--- board.py
+++ board.py
@@ -2,10 +2,10 @@ from xmlrpc.client import boolean
 from copy import deepcopy
 
 class Board():
-  months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-  dates = [i for i in range(1, 32)]
-  days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"]
-  labels = months + dates + days
+  # months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+  # dates = [i for i in range(1, 32)]
+  # days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"]
+  # labels = months + dates + days
 
   def __init__(self):
     self.remaining_locations = []
@@ -32,9 +32,9 @@ class Board():
                 # label_index += 1
             row.append(value)
         self.matrix.append(row)
-    self.matrix[0][1] = "D"
-    self.matrix[5][3] = "D"
-    self.matrix[7][5] = "D"
+    # self.matrix[0][1] = "D"
+    # self.matrix[5][3] = "D"
+    # self.matrix[7][5] = "D"
     #self.print()
 
 
@@ -44,14 +44,51 @@ class Board():
   def set(self, matrix):
     self.matrix = matrix
 
+  def place_piece(self, piece):
+    self.find_remaining()
+    placed = False
+    while not placed:
+      try: 
+          location = self.next_location()
+      except:
+        break
+      placed = self.overlay_piece(location[0], location[1], piece)
+    return placed
+
+  def overlay_piece(self, row: int, col: int, p):
+      # make a copy so we can undo if the piece doesn't fit in the row, col
+      matrix = deepcopy(self.get())
+      
+      for i in range(len(p)):
+          for j in range(len(p[0])):
+            try: # this should catch invalid matrix locations (ie.. placing a piece too close to the edge)
+              if p[i][j] == "-":
+                continue
+              else:
+                if matrix[row + i][col + j] == "X":
+                  return False
+                elif matrix[row + i][col + j] == "D":
+                  return False
+                elif isinstance(matrix[row + i][col + j] , int):
+                  return False
+                else: matrix[row + i][col + j] = int(p[i][j])
+            except:
+              return
+
+      # Add function to check for void state and return boolean
+
+      self.set(matrix)
+      return True
+
   def find_remaining(self):
     self.remaining_locations = []
     for i in range(0, 8):
       for j in range(0, 7):
-          if self.matrix[i][j] == "-":
-            self.remaining_locations.append([i, j])
-          if self.matrix[i][j] == "D":
-            self.remaining_locations.append([i, j])
+        self.remaining_locations.append([i, j])
+          # if self.matrix[i][j] == "-":
+          #   self.remaining_locations.append([i, j])
+          # if self.matrix[i][j] == "D":
+          #   self.remaining_locations.append([i, j])
 
   def is_isolated(self, copy) -> boolean:
     pass
@@ -86,6 +123,6 @@ class Board():
   def print(self):
     for i in range(8):
       for j in range(7):
-          print(self.matrix[i][j], end=" ", file=open('output.txt', 'a'))
-      print("", file=open('output.txt', 'a'))
-    print("\n", file=open('output.txt', 'a'))
+          print(self.matrix[i][j], end=" ")
+      print("")
+    print("\n")
--- main.py
+++ main.py
@@ -1,16 +1,38 @@
+from array import array
+from inspect import stack
 import random as r
 import numpy as np
 from copy import deepcopy
 import itertools
 from piece import Piece
 from board import Board
-
+import sys
+print(sys.getrecursionlimit())
 class Game():
-    def __init__(self, order):
+    def __init__(self):
       self.pieces = {}
-      self.order = order
+      self.order_permutations = list(itertools.permutations([0,1,2,3,4,5,6,7,8,9],10))
       self.generate_pieces()
-      self.order_rotations = self.generate_order_rotations()
+      self.stack = []
+
+      # order of the pieces
+      self.order = []
+      
+
+      # index of the current piece
+      self.position = 0
+
+
+      self.state = {}
+      
+
+    def next_order(self) -> list:
+      self.order = list(self.order_permutations.pop(r.randrange(0, len(self.order_permutations))))
+
+    def init_state(self):
+      for i in self.order:
+        self.state[i] = {'cur': 0, 'max': len(self.pieces[i]) - 1}
+      print(self.state)
 
     def generate_pieces(self):
       piece_defs = ["000|00-","11|1-|1-|1-", "22|2-|22", "333|3--","--4|444|4--", "-55|55-", "666|-6-|-6-", "7--|7--|777", "-8|88|8-|8-", "9999"]
@@ -60,89 +82,71 @@ class Game():
         #     print("\n")
         # print("-"*15,"\n")
 
-    def remove_piece(self, i):
-      self.pieces.pop(i)
-
-    def overlay_piece(self, row, col, p):
-      matrix = deepcopy(self.b.get())
-      
-      for i in range(len(p)):
-          for j in range(len(p[0])):
-            try: # this should catch invalid matrix locations (ie.. placing a piece too close to the edge)
-              if p[i][j] == "-":
-                continue
-              else:
-                if matrix[row + i][col + j] == "X":
-                  return False
-                elif matrix[row + i][col + j] == "D":
-                  return False
-                elif isinstance(matrix[row + i][col + j] , int):
-                  return False
-                else: matrix[row + i][col + j] = int(p[i][j])
-            except:
-              return
-      self.b.set(matrix)
-      return True
-
-    def get_order(self) -> list:
-      return list(self.piece_order_permutations.pop(r.randrange(0, len(self.piece_order_permutations))))
-
-    def generate_order_rotations(self) -> list:
-      indices = {}
-      for i in self.pieces:
-        for j in range(len(self.pieces[i])):
-          indices.setdefault(i, []).append(j)
-      return list(itertools.product(*indices.values()))
 
     def get_piece(self, key, rotation):
       return self.pieces[key][rotation]
 
 
-    def place_pieces(self, runs = 1):
-      print("Call #{} with order {}".format(runs, self.order), file=open('output.txt', 'a'))
-      rotations = self.order_rotations
+    def solver(self):
+      self.next_order()
+      self.init_state()
+      self.position = 0
+
+      # Initialize with a blank board
+      if len(self.stack) == 0:
+        self.stack.append(Board())
+
+
+      while self.position <= len(self.pieces) - 1:
+        # Get the latest board from the stack
+        b = deepcopy(self.stack[-1])
+
+        b.print()
+
+        current_piece = self.order[self.position]
+        current_rotation = self.state[self.order[self.position]]['cur']
+
+        piece = self.get_piece(current_piece,current_rotation)
+
+        placed = b.place_piece(piece)
+
+        if placed: 
+          self.position += 1
+          self.stack.append(b)
+
+        if not placed: 
+          self.state[self.order[self.position]]['cur'] += 1
+
+        if not placed and self.state[self.order[self.position]]['cur'] >= self.state[self.order[self.position]]['max']: # backtrack
+          print("start backtracking...")
+          self.set_position()
+
+          
+          self.state[self.order[self.position]]['cur'] += 1
+
+          print("end backtrack")
+
+      self.solver()
+
+    def set_position(self):
       
+      if self.state[self.order[self.position]]['cur'] >= self.state[self.order[self.position]]['max']:
+        self.position -= 1
+        self.stack.pop()
+        self.set_position()
 
-      #print("order: ", order)
-      for j in rotations: # j is a list
-        # print("With piece rotation {}".format(j), file=open('output.txt', 'a'))
-        self.b = Board()
-        remaining = deepcopy(self.order)
-        for i in self.order: # i is an int
-          self.b.find_remaining()
-          piece = self.get_piece(i, j[i])
-          placed = False
-          while not placed:
-            try: 
-                location = self.b.next_location()
-            except:
-              break
-            placed = self.overlay_piece(location[0], location[1], piece)
-          if (placed):
-            self.b.print()
-            self.b.mark_filled()
-            remaining.pop(remaining.index(i))
-          if len(remaining) == 0:
-            print("Winner! With piece order {} and rotation {}".format(j, self.order), file=open('output.txt', 'a'))
-            self.b.print()
-          #print(np.array(piece))
+      print(self.order[self.position])
+      print(self.order)
+      # reset remaining to initial rotation
+      pos = self.position
+      while pos + 1 < len(self.order):
 
-      self.place_pieces(runs + 1)         
+        self.state[self.order[pos + 1]]['cur'] = 0
 
-
-
+        pos += 1
 
 if __name__ == "__main__":
-  piece_order_permutations = list(itertools.permutations([0,1,2,3,4,5,6,7,8,9],10))
+  g = Game()
+  g.solver()
 
-  def get_order() -> list:
-    return list(piece_order_permutations.pop(r.randrange(0, len(piece_order_permutations))))
-    #return list(piece_order_permutations.pop())
-
-  def do_game():
-    g = Game(get_order())
-    g.place_pieces()
-
-  while piece_order_permutations:
-    do_game()
 
